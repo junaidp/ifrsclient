e7f0c5312d3fec23ed348570da09e91d0d3d65f4
junaid paracha <junaidp@gmail.com>
2019-11-26 20:52:25 +0100

    express

--- server.js
+++ server.js
@@ -13,4 +13,7 @@ res.sendFile(path.join(__dirname,'/dist/ifrscli/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT);
+//app.listen(process.env.PORT);
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  });
