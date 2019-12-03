f0a9b1ec2117cc71c47f0179b371e40b6f3618fd
unknown <hamzariaz1994@gmail.com>
2019-12-03 17:00:22 +0500

    added alert on login in login component (hamza)

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -3,7 +3,7 @@ import { Loginservice } from "./Loginservice";
 import { Globals } from "../globals";
 import { Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-
+import {ViewChild, ElementRef } from '@angular/core';
 import { AuthService } from '../auth.service';
 @Component({
   selector: "app-login",
@@ -11,6 +11,8 @@ import { AuthService } from '../auth.service';
   styleUrls: ["./login.component.css"]
 })
 export class LoginComponent implements OnInit {
+  isLoggedIn = true;
+  @ViewChild('alert', { static: true }) alert: ElementRef;
   constructor(public loginservice: Loginservice, public globals: Globals, private router: Router, public authService: AuthService) { }
   returnUrl: string;
   name = "";
@@ -29,14 +31,17 @@ export class LoginComponent implements OnInit {
   ngOnInit() {
     this.returnUrl = '/home';
     this.authService.logout();
+
+
   }
+ 
   login() {
     var data = {
       name: this.name,
       password: this.password
     };
     this.loginservice.signIn(data).then(response => {
-      alert(JSON.stringify(response.data))
+      
       if (data.name == "shehryar" && data.password == "password") {
         alert("successful login")
 
@@ -48,14 +53,15 @@ export class LoginComponent implements OnInit {
         this.router.navigate([this.returnUrl]);
       }
 
-      else if (data.name == '') {
-        alert("Please Enter User and Pasword")
+     else{
+      this.isLoggedIn = false
+   
 
-      }
-      else {
-        alert("wrong Username or password")
-
-      }
+     }
     });
+  }
+  closeAlert() {
+  this.isLoggedIn = true
+   // this.alert.nativeElement.classList.remove('show');
   }
 }
