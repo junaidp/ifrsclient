71fd35ec689b5bd9dc9ae81e3d7060011d5665aa
unknown <hamzariaz1994@gmail.com>
2019-12-06 19:03:30 +0500

    edited jquery of newlease

--- src/app/new-lease/new-lease.component.ts
+++ src/app/new-lease/new-lease.component.ts
@@ -110,83 +110,364 @@ export class NewLeaseComponent implements OnInit {
   }
   ngOnInit() {
 
-
-
     $(document).ready(function () {
-      var navListItems = $("div.setup-panel div a"),
-        allWells = $(".setup-content"),
-        allNextBtn = $(".nextBtn");
 
+      var navListItems = $('div.setup-panel div a'),
+              allWells = $('.setup-content'),
+              allNextBtn = $('.nextBtn');
+  
       allWells.hide();
-
+  
       navListItems.click(function (e) {
-        e.preventDefault();
-        var $target = $($(this).attr("href")),
-          $item = $(this);
-
-        if (!$item.hasClass("disabled")) {
-          navListItems.removeClass("btn-primary").addClass("btn-default");
-          $item.addClass("btn-primary");
-          allWells.hide();
-          $target.show();
-          $target.find("input:eq(0)").focus();
-        }
-      });
-
-      allNextBtn.click(function () {
-        var curStep = $(this).closest(".setup-content"),
-          curStepBtn = curStep.attr("id"),
-          nextStepWizard = $(
-            'div.setup-panel div a[href="#' + curStepBtn + '"]'
-          )
-            .parent()
-            .next()
-            .children("a"),
-          curInputs = curStep.find("input[type='text'],input[type='url']"),
-          isValid = true;
-        debugger;
-        if (curStepBtn == "step-1") {
-          $(".new_lease1").css("display", "block");
-          $(".new_lease").css("display", "none");
-          $(".new_lease2").css("display", "none");
-          $(".new_lease3").css("display", "none");
-          $(".new_lease4").css("display", "none");
-        }
-        if (curStepBtn == "step-2") {
-          $(".new_lease2").css("display", "block");
-          $(".new_lease").css("display", "none");
-          $(".new_lease1").css("display", "none");
-          $(".new_lease3").css("display", "none");
-          $(".new_lease4").css("display", "none");
-        }
-        if (curStepBtn == "step-3") {
-          $(".new_lease3").css("display", "block");
-          $(".new_lease").css("display", "none");
-          $(".new_lease1").css("display", "none");
-          $(".new_lease2").css("display", "none");
-          $(".new_lease4").css("display", "none");
-        }
-        if (curStepBtn == "step-4") {
-          $(".new_lease4").css("display", "block");
-          $(".new_lease").css("display", "none");
-          $(".new_lease1").css("display", "none");
-          $(".new_lease3").css("display", "none");
-          $(".new_lease2").css("display", "none");
-        }
-        $(".form-group").removeClass("has-error");
-        for (var i = 0; i < curInputs.length; i++) {
-          if (!curInputs[i].validity.valid) {
-            isValid = false;
-            $(curInputs[i])
-              .closest(".form-group")
-              .addClass("has-error");
+          e.preventDefault();
+          var $target = $($(this).attr('href')), $item = $(this);
+  
+          if (!$item.hasClass('disabled')) {
+              navListItems.removeClass('btn-primary').addClass('btn-default');
+              $item.addClass('btn-primary');
+              allWells.hide();
+              $target.show();
+              $target.find('input:eq(0)').focus();
           }
-        }
-
-        if (isValid) debugger;
-        nextStepWizard.removeAttr("disabled").trigger("click");
       });
-      $("div.setup-panel div a.btn-primary").trigger("click");
-    });
+  
+      allNextBtn.click(function(){
+          var curStep = $(this).closest(".setup-content"),
+              curStepBtn = curStep.attr("id"),
+              nextStepWizard = $('div.setup-panel div a[href="#' + curStepBtn + '"]').parent().next().children("a"),
+              curInputs = curStep.find("input[type='text'],input[type='number'],input[type='url']"),
+              isValid = true;
+              
+          $(".form-group").removeClass("has-error");
+  
+          for(var i=0; i<curInputs.length; i++){
+              if (!curInputs[i].validity.valid){
+                  isValid = false;
+                  $(curInputs[i]).closest(".form-group").addClass("has-error");
+                  
+              }
+          }
+          if(!isValid){
+              alert('Fill all Fields');
+          }
+          
+          if (isValid){
+              nextStepWizard.removeAttr('disabled').trigger('click');
+              
+              if(curStepBtn == "step-1"){
+                  $('.new_lease1').css('display','block');
+                  $('.new_lease').css('display','none');
+                  $('.new_lease2').css('display','none');
+                  $('.new_lease3').css('display','none');
+                  $('.new_lease4').css('display','none');
+                  $('.bolder').css('font-weight','normal');
+                  $('.bolder1').css('font-weight','bolder');
+                  $('.bolder2').css('font-weight','normal');
+                  $('.bolder3').css('font-weight','normal');
+                  $('.bolder4').css('font-weight','normal');
+              }
+              if(curStepBtn == "step-2"){
+                  $('.new_lease2').css('display','block');
+                  $('.new_lease').css('display','none');
+                  $('.new_lease1').css('display','none');
+                  $('.new_lease3').css('display','none');
+                  $('.new_lease4').css('display','none');
+                  $('.bolder').css('font-weight','normal');
+                  $('.bolder2').css('font-weight','bolder');
+                  $('.bolder1').css('font-weight','normal');
+                  $('.bolder3').css('font-weight','normal');
+                  $('.bolder4').css('font-weight','normal');
+              }
+              if(curStepBtn == "step-3"){
+                  $('.new_lease3').css('display','block');
+                  $('.new_lease').css('display','none');
+                  $('.new_lease1').css('display','none');
+                  $('.new_lease2').css('display','none');
+                  $('.new_lease4').css('display','none');
+                  $('.bolder').css('font-weight','normal');
+                  $('.bolder3').css('font-weight','bolder');
+                  $('.bolder1').css('font-weight','normal');
+                  $('.bolder2').css('font-weight','normal');
+                  $('.bolder4').css('font-weight','normal');
+              }
+              if(curStepBtn == "step-4"){
+                  $('.new_lease4').css('display','block');
+                  $('.new_lease').css('display','none');
+                  $('.new_lease1').css('display','none');
+                  $('.new_lease3').css('display','none');
+                  $('.new_lease2').css('display','none');
+                  $('.bolder').css('font-weight','normal');
+                  $('.bolder4').css('font-weight','bolder');
+                  $('.bolder1').css('font-weight','normal');
+                  $('.bolder3').css('font-weight','normal');
+                  $('.bolder2').css('font-weight','normal');
+              }
+          } 
+      });
+      $('div.setup-panel div a.btn-primary').trigger('click');
+      
+  });
+  var modal = document.getElementById("myModal");
+  
+  // Get the button that opens the modal
+  var btn = document.getElementById("myBtn");
+
+  // Get the <span> element that closes the modal
+  var span = document.getElementsByClassName("closes")[0];
+
+  // When the user clicks the button, open the modal 
+  btn.onclick = function() {
+  modal.style.display = "block";
   }
+
+  // When the user clicks on <span> (x), close the modal
+  // span.onclick = function() {
+  // modal.style.display = "none";
+  // }
+
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = function(event) {
+  if (event.target == modal) {
+      modal.style.display = "none";
+  }
+  }
+
+function show(select_item) {
+if (select_item == 1) {
+    document.getElementById('hiddenDiv').style.visibility='visible';
+    document.getElementById('hiddenDiv').style.display='block';
+    document.getElementById('lease_no').style.display='none';
+    document.getElementById('lease_no').style.visibility='hidden';
+} 
+else if(select_item == 0){
+  document.getElementById('hiddenDiv').style.visibility='hidden';
+  document.getElementById('hiddenDiv').style.display='none';
+  document.getElementById('lease_no').style.display='block'
+  document.getElementById('lease_no').style.visibility='visible';
+  document.getElementById('hiddenDiv1').style.visibility='hidden';
+  document.getElementById('hiddenDiv1').style.display='none';
+  document.getElementById('hiddenDiv2').style.visibility='hidden';
+  document.getElementById('hiddenDiv2').style.display='none';
+  document.getElementById('hiddenDiv3').style.visibility='hidden';
+  document.getElementById('hiddenDiv3').style.display='none';
+  document.getElementById('hiddenDiv4').style.visibility='hidden';
+  document.getElementById('hiddenDiv4').style.display='none';
+  document.getElementById('hiddenDiv5').style.visibility='hidden';
+  document.getElementById('hiddenDiv5').style.display='none';
+}
+}
+function show1(select_item) {
+  if (select_item == 1) {
+    document.getElementById('hiddenDiv1').style.visibility='visible';
+    document.getElementById('hiddenDiv1').style.display='block';
+    document.getElementById('lease_no').style.display='none';
+    document.getElementById('lease_no').style.visibility='hidden';
+    
+      
+  } 
+  else if(select_item == 0){
+    document.getElementById('hiddenDiv1').style.visibility='hidden';
+    document.getElementById('hiddenDiv1').style.display='none';
+    document.getElementById('lease_no').style.display='block'
+    document.getElementById('lease_no').style.visibility='visible';
+  }
+}
+function show2(select_item) {
+  if (select_item == 0) {
+      document.getElementById('hiddenDiv2').style.visibility='visible';
+      document.getElementById('hiddenDiv2').style.display='block';
+      document.getElementById('lease_no').style.display='none';
+      document.getElementById('lease_no').style.visibility='hidden';
+  } 
+  else if (select_item == 1){
+      document.getElementById('hiddenDiv2').style.visibility='hidden';
+      document.getElementById('hiddenDiv2').style.display='none';
+      document.getElementById('lease_no').style.display='block'
+      document.getElementById('lease_no').style.visibility='visible';
+  }
+}
+function show3(select_item) {
+  if (select_item == 1) {
+      document.getElementById('hiddenDiv3').style.visibility='visible';
+      document.getElementById('hiddenDiv3').style.display='block';
+      document.getElementById('lease_no').style.display='none';
+      document.getElementById('lease_no').style.visibility='hidden';
+  } 
+  else if(select_item == 0){
+      document.getElementById('hiddenDiv3').style.visibility='hidden';
+      document.getElementById('hiddenDiv3').style.display='none';
+      document.getElementById('lease_no').style.display='block'
+      document.getElementById('lease_no').style.visibility='visible';
+  }
+}
+function show4(select_item) {
+  if (select_item == 1) {
+     
+      document.getElementById('hiddenDiv4').style.visibility='visible';
+      document.getElementById('hiddenDiv4').style.display='block';
+      document.getElementById('lease_no').style.display='none';
+      document.getElementById('lease_no').style.visibility='hidden';
+  } 
+  else{
+    
+      document.getElementById('hiddenDiv4').style.visibility='hidden';
+      document.getElementById('hiddenDiv4').style.display='none';
+      document.getElementById('lease_no').style.display='block'
+      document.getElementById('lease_no').style.visibility='visible';
+  }
+}
+function show5(select_item) {
+  if (select_item == 1) {
+    
+      document.getElementById('hiddenDiv4').style.visibility='visible';
+      document.getElementById('hiddenDiv4').style.display='block';
+      document.getElementById('lease_no').style.display='none';
+      document.getElementById('lease_no').style.visibility='hidden';
+  } 
+  else if(select_item == 0) {
+    document.getElementById('hiddenDiv5').style.visibility='hidden';
+    document.getElementById('hiddenDiv5').style.display='none';
+    document.getElementById('lease_no').style.display='block'
+    document.getElementById('lease_no').style.visibility='visible';
+    document.getElementById('lease_yes').style.visibility='hidden';
+    document.getElementById('lease_yes').style.display='none';
+  }
+  else if(select_item == 2) {
+      document.getElementById('hiddenDiv5').style.visibility='hidden';
+      document.getElementById('hiddenDiv5').style.display='none';
+      document.getElementById('lease_yes').style.display='block'
+      document.getElementById('lease_yes').style.visibility='visible';
+      document.getElementById('lease_no').style.visibility='hidden';
+      document.getElementById('lease_no').style.display='none';
+  }
+}
+function show6(select_item) {
+  if (select_item == 1) {
+    document.getElementById('hiddenDiv5').style.visibility='visible';
+    document.getElementById('hiddenDiv5').style.display='block';
+  } 
+  else if (select_item == 0){
+    document.getElementById('lease_yes').style.visibility='hidden';
+    document.getElementById('lease_yes').style.display='visible';
+    document.getElementById('hiddenDiv5').style.visibility='hidden';
+    document.getElementById('hiddenDiv5').style.display='none';
+    document.getElementById('lease_no').style.visibility='visible';
+    document.getElementById('lease_no').style.display='block';
+  }
+}
+function show7(select_item) {
+  if (select_item == 1) {
+    document.getElementById('lease_yes').style.visibility='visible';
+      document.getElementById('lease_yes').style.display='block';
+      document.getElementById('lease_no').style.visibility='hidden';
+      document.getElementById('lease_no').style.display='none';
+  } 
+  else if (select_item == 0){
+    document.getElementById('lease_no').style.visibility='visible';
+    document.getElementById('lease_no').style.display='block'
+      document.getElementById('lease_yes').style.visibility='hidden';
+      document.getElementById('lease_yes').style.display='none';
+  }
+}
+function showpopup(select_item) {
+  if (select_item == 1) {
+    document.getElementById('myModal').style.visibility='visible';
+    document.getElementById('myModal').style.display='block';
+  } 
+}
+function closess(){
+document.getElementById('myModal').style.display='none';
+}
+function showpopupq1() {
+document.getElementById('question_popup1').style.visibility='visible';
+document.getElementById('question_popup1').style.display='block';
+}
+function closes(){
+document.getElementById('question_popup1').style.display='none';
+}
+function showpopupq2() {
+document.getElementById('question_popup2').style.visibility='visible';
+document.getElementById('question_popup2').style.display='block';
+}
+function showpopupq3() {
+document.getElementById('question_popup3').style.visibility='visible';
+document.getElementById('question_popup3').style.display='block';
+}
+function showpopupq4() {
+document.getElementById('question_popup4').style.visibility='visible';
+document.getElementById('question_popup4').style.display='block';
+}
+function showpopupq5() {
+document.getElementById('question_popup5').style.visibility='visible';
+document.getElementById('question_popup5').style.display='block';
+}
+function showpopupq6() {
+document.getElementById('question_popup6').style.visibility='visible';
+document.getElementById('question_popup6').style.display='block';
+}
+function showpopupq7() {
+document.getElementById('question_popup7').style.visibility='visible';
+document.getElementById('question_popup7').style.display='block';
+}
+function shownext(){
+  $(".shownext1").toggle("slow");
+}
+function shownext1(){
+  $(".shownextq5").toggle("slow");
+}
+function closes4(){
+document.getElementById('question_popup4').style.display='none';
+}
+function closes1(){
+document.getElementById('question_popup1').style.display='none';
+}
+function closes2(){
+document.getElementById('question_popup2').style.display='none';
+}
+function closes3(){
+document.getElementById('question_popup3').style.display='none';
+}
+function closes5(){
+document.getElementById('question_popup5').style.display='none';
+}
+function closes6(){
+document.getElementById('question_popup6').style.display='none';
+}
+function closes7(){
+document.getElementById('question_popup7').style.display='none';
+}
+function journal_entry_dis(){
+  $(".journal_display").togle("slow");
+}
+
+
+  
+  // function readURL(input) {
+  //     if (input.files && input.files[0]) {
+  //         var reader = new FileReader();
+  
+  //         reader.onload = function (e) {
+  //             $('#blah')
+  //                 .attr('src', e.target.result);
+  //         };
+  
+  //         reader.readAsDataURL(input.files[0]);
+  //     }
+  // }
+  
+  // function Validate() {
+  //     var password = document.getElementById("txtPassword").value;
+  //     var confirmPassword = document.getElementById("txtConfirmPassword").value;
+  //     if (password != confirmPassword) {
+  //         alert("Passwords do not match.");
+  //         return false;
+  //     }
+  //     return true;
+  // }
+  
+
+     
+  
+  }
+
 }
