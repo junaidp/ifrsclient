3d9913b10aedcfd68d05b5845ef6aa14f2e6c5db
Adnan Khokhar <45161899+Adnankhokhar747@users.noreply.github.com>
2019-12-12 13:54:42 +0500

    update

--- src/assets/js/jquery.js
+++ src/assets/js/jquery.js
@@ -133,9 +133,7 @@ $(document).ready(function (){
     }
 
     // When the user clicks on <span> (x), close the modal
-    span.onclick = function() {
-    modal.style.display = "none";
-    }
+    
 
     // When the user clicks anywhere outside of the modal, close it
     window.onclick = function(event) {
