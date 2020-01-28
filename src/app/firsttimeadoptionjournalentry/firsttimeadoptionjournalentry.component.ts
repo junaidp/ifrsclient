25f331555aa755934178edee9e53b8a90e6e69cb
unknown <hamzariaz1994@gmail.com>
2020-01-28 15:30:19 +0500

    first time adoption view

--- src/app/firsttimeadoptionjournalentry/firsttimeadoptionjournalentry.component.ts
+++ src/app/firsttimeadoptionjournalentry/firsttimeadoptionjournalentry.component.ts
@@ -5,7 +5,7 @@ import { Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import {ViewChild, ElementRef } from '@angular/core';
 import { allResolved } from "q";
-
+declare var $: any;
 @Component({
   selector: "app-firsttimeadoptionjournalentry",
   templateUrl: "./firsttimeadoptionjournalentry.component.html",
@@ -17,7 +17,10 @@ export class FirsttimeadoptionjournalentryComponent implements OnInit {
 
    
    ngOnInit() {
-
+    $("#journal_entry_show").on("click", function (){
+      $("#journal_entry").toggle("show");
+      $("#journal_entry1").toggle("show");
+  });
      }
  
     SignUp(){
