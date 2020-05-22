3cd8315c8fbb7b7f07d32f95141ba4f519c65778
hamza riaz <hamzariaz1994@gmail.com>
2020-05-22 12:36:18 +0500

    url changed

--- src/app/globals.ts
+++ src/app/globals.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class Globals {
-  APP_URL  = "http://57d1c5bb.ngrok.io/";
+  APP_URL  = "http://19ae1e77.ngrok.io/";
   //APP_URL  = "//7d83bbbe.ngrok.io";
   APP_URL_HEROKU = "//compliancetool.herokuapp.com";
   userId: string = '';
