446d9758814125a0616ea8b982724f60dd38c52c
Adnan Khokhar <45161899+Adnankhokhar747@users.noreply.github.com>
2020-09-28 14:52:10 +0500

    login form

--- src/assets/landing/js/custom.js
+++ src/assets/landing/js/custom.js
@@ -1,168 +1,168 @@
 (function($) {
 
-  // Add smooth scrolling to all links in navbar
-  $(".navbar a,a.btn-appoint, .quick-info li a, .overlay-detail a").on('click', function(event) {
 
-    var hash = this.hash;
-    if (hash) {
-      event.preventDefault();
-      $('html, body').animate({
-        scrollTop: $(hash).offset().top
-      }, 900, function() {
-        window.location.hash = hash;
-      });
-    }
+    // Add smooth scrolling to all links in navbar
+    $(".navbar a,a.btn-appoint, .quick-info li a, .overlay-detail a").on('click', function(event) {
 
-  });
+        var hash = this.hash;
+        if (hash) {
+            event.preventDefault();
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 900, function() {
+                window.location.hash = hash;
+            });
+        }
 
-  $(".navbar-collapse a").on('click', function() {
-    $(".navbar-collapse.collapse").removeClass('in');
-  });
+    });
 
-  //jQuery to collapse the navbar on scroll
-  $(window).scroll(function() {
-    if ($(".navbar-default").offset().top > 50) {
-      $(".navbar-fixed-top").addClass("top-nav-collapse");
-    } else {
-      $(".navbar-fixed-top").removeClass("top-nav-collapse");
-    }
-  });
+    $(".navbar-collapse a").on('click', function() {
+        $(".navbar-collapse.collapse").removeClass('in');
+    });
+
+    //jQuery to collapse the navbar on scroll
+    $(window).scroll(function() {
+        if ($(".navbar-default").offset().top > 50) {
+            $(".navbar-fixed-top").addClass("top-nav-collapse");
+        } else {
+            $(".navbar-fixed-top").removeClass("top-nav-collapse");
+        }
+    });
 
 })(jQuery);
 
 function readURL(input) {
-  if (input.files && input.files[0]) {
-    var reader = new FileReader();
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
 
-    reader.onload = function (e) {
-        $('#blah')
-            .attr('src', e.target.result);
-    };
+        reader.onload = function(e) {
+            $('#blah')
+                .attr('src', e.target.result);
+        };
 
-    reader.readAsDataURL(input.files[0]);
-  }
+        reader.readAsDataURL(input.files[0]);
+    }
 }
 
-$(document).ready(function () {
-  var modal = document.getElementById("apmodal");
-  var btn = document.getElementById("myBtn");
-  var span = document.getElementsByClassName("close")[0];
-  btn.onclick = function() {
-    modal.style.display = "block";
-  }
-  span.onclick = function() {
-    modal.style.display = "none";
-  }
-  window.onclick = function(event) {
-    if (event.target == modal) {
-      modal.style.display = "none";
+$(document).ready(function() {
+    var modal = document.getElementById("apmodal");
+    var btn = document.getElementById("myBtn");
+    var span = document.getElementsByClassName("close")[0];
+    btn.onclick = function() {
+        modal.style.display = "block";
     }
-  }
-});
-$(document).ready(function () {
-  var modal = document.getElementById("apmodal");
-  var btn = document.getElementById("gold");
-  var span = document.getElementsByClassName("close")[0];
-  btn.onclick = function() {
-    modal.style.display = "block";
-  }
-  span.onclick = function() {
-    modal.style.display = "none";
-  }
-  window.onclick = function(event) {
-    if (event.target == modal) {
-      modal.style.display = "none";
+    span.onclick = function() {
+        modal.style.display = "none";
     }
-  }
-});
-$(document).ready(function () {
-  var modal = document.getElementById("apmodal");
-  var btn = document.getElementById("silver");
-  var span = document.getElementsByClassName("close")[0];
-  btn.onclick = function() {
-    modal.style.display = "block";
-  }
-  span.onclick = function() {
-    modal.style.display = "none";
-  }
-  window.onclick = function(event) {
-    if (event.target == modal) {
-      modal.style.display = "none";
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
     }
-  }
 });
-$(document).ready(function () {
-  var modal = document.getElementById("apmodallogin");
-  var btn = document.getElementById("login");
-  var span = document.getElementsByClassName("close1")[0];
-  btn.onclick = function() {
-    modal.style.display = "block";
-  }
-  span.onclick = function() {
-    modal.style.display = "none";
-  }
-  window.onclick = function(event) {
-    if (event.target == modal) {
-      modal.style.display = "none";
+$(document).ready(function() {
+    var modal = document.getElementById("apmodal");
+    var btn = document.getElementById("gold");
+    var span = document.getElementsByClassName("close")[0];
+    btn.onclick = function() {
+        modal.style.display = "block";
     }
-  }
-});
-$(document).ready(function () {
-  var modal = document.getElementById("apmodallogin");
-  var btn = document.getElementById("login1");
-  var span = document.getElementsByClassName("close1")[0];
-  btn.onclick = function() {
-    modal.style.display = "block";
-  }
-  span.onclick = function() {
-    modal.style.display = "none";
-  }
-  window.onclick = function(event) {
-    if (event.target == modal) {
-      modal.style.display = "none";
+    span.onclick = function() {
+        modal.style.display = "none";
     }
-  }
-});
-$(document).ready(function () {
-  var modal = document.getElementById("apmodal");
-  var btn = document.getElementById("aboutt");
-  btn.onclick = function() {
-    modal.style.display = "block";
-  }
-  window.onclick = function(event) {
-    if (event.target == modal) {
-      modal.style.display = "none";
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
     }
-  }
 });
-$(document).ready(function () {
-  var modal = document.getElementById("companylogin");
-  var btn = document.getElementById("companyloginclick");
-  var span = document.getElementsByClassName("close2")[0];
-  btn.onclick = function() {
-    modal.style.display = "block";
-  }
-  span.onclick = function() {
-    modal.style.display = "none";
-  }
-  window.onclick = function(event) {
-    if (event.target == modal) {
-      modal.style.display = "none";
+$(document).ready(function() {
+    var modal = document.getElementById("apmodal");
+    var btn = document.getElementById("silver");
+    var span = document.getElementsByClassName("close")[0];
+    btn.onclick = function() {
+        modal.style.display = "block";
     }
-  }
+    span.onclick = function() {
+        modal.style.display = "none";
+    }
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
 });
-$(document).ready(function () {
-  var modal = document.getElementById("companylogin");
-  var btn = document.getElementById("companyloginclick1");
-  btn.onclick = function() {
-    modal.style.display = "block";
-  }
+$(document).ready(function() {
+    var modal = document.getElementById("apmodallogin");
+    var btn = document.getElementById("login");
+    var span = document.getElementsByClassName("close1")[0];
+    btn.onclick = function() {
+        modal.style.display = "block";
+    }
+    span.onclick = function() {
+        modal.style.display = "none";
+    }
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
 });
-$(document).ready(function () {
-  var modal = document.getElementById("companylogin");
-  var btn = document.getElementById("companyloginclick2");
-  btn.onclick = function() {
-    modal.style.display = "block";
-  }
+$(document).ready(function() {
+    var modal = document.getElementById("apmodallogin");
+    var btn = document.getElementById("login1");
+    var span = document.getElementsByClassName("close1")[0];
+    btn.onclick = function() {
+        modal.style.display = "block";
+    }
+    span.onclick = function() {
+        modal.style.display = "none";
+    }
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
 });
-
+$(document).ready(function() {
+    var modal = document.getElementById("apmodal");
+    var btn = document.getElementById("aboutt");
+    btn.onclick = function() {
+        modal.style.display = "block";
+    }
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+});
+$(document).ready(function() {
+    var modal = document.getElementById("companylogin");
+    var btn = document.getElementById("companyloginclick");
+    var span = document.getElementsByClassName("close2")[0];
+    btn.onclick = function() {
+        modal.style.display = "block";
+    }
+    span.onclick = function() {
+        modal.style.display = "none";
+    }
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+});
+$(document).ready(function() {
+    var modal = document.getElementById("companylogin");
+    var btn = document.getElementById("companyloginclick1");
+    btn.onclick = function() {
+        modal.style.display = "block";
+    }
+});
+$(document).ready(function() {
+    var modal = document.getElementById("companylogin");
+    var btn = document.getElementById("companyloginclick2");
+    btn.onclick = function() {
+        modal.style.display = "block";
+    }
+});
