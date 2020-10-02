8e19359a614f446b87b1372f8fb93fc7aec5dc73
unknown <hamzariaz1994@gmail.com>
2020-10-02 12:18:28 +0500

    global link changed

--- src/app/globals.ts
+++ src/app/globals.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class Globals {
-  APP_URL  = "http://90965abbd92a.ngrok.io/";
+  APP_URL  = "http://4e61c2108e2e.ngrok.io/";
   //APP_URL  = "//7d83bbbe.ngrok.io";
   APP_URL_HEROKU = "//compliancetool.herokuapp.com";
   userId: string = '';
