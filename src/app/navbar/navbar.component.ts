e4bae34821c67840b455406c45ecc852624ee0e0
Adnan Khokhar <45161899+Adnankhokhar747@users.noreply.github.com>
2020-11-03 11:01:54 +0500

    add new user form and payment method remove

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -30,7 +30,17 @@ export class NavbarComponent implements OnInit {
     else{
       $('#addUSerOption').hide();
     }
-   
+    var modal = document.getElementById("myModal");
+    var btn = document.getElementById("myBtn");
+    var span = document.getElementsByClassName("close")[0];
+    btn.onclick = function() {
+      modal.style.display = "block";
+    }
+    window.onclick = function(event) {
+      if (event.target == modal) {
+        modal.style.display = "none";
+      }
+    }
   }
   
 
