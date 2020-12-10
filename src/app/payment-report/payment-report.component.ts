c7e729ad301050a8227e011f8af61c2e0af7d1a8
unknown <hamzariaz1994@gmail.com>
2020-12-10 23:52:29 +0500

    payment report initial done

--- src/app/payment-report/payment-report.component.ts
+++ src/app/payment-report/payment-report.component.ts
@@ -4,6 +4,8 @@ import { paymentService } from "./paymentService";
 import { rightService } from "src/app/right-of-use/rightService";
 import { ThrowStmt } from '@angular/compiler';
 import { JsonPipe } from '@angular/common';
+import { NgxSpinnerService } from "ngx-spinner";
+
 import { JournalService } from '../journal-entries/journalService';
 import { JournalEntriesComponent } from '../journal-entries/journal-entries.component';
 declare var $: any;
@@ -27,17 +29,58 @@ export class PaymentReportComponent implements OnInit {
   finalDate
   dateSelectorMonth
 
-  constructor(public paymentService: paymentService, public globals: Globals, public rightService: rightService, public journalService: JournalService) { }
-  mapUserData: Map<string, Map<string, string>>;
-  mapUserFilter: Map<string, Map<string, string>>;
 
+  leaseContractNo = "";
+  classAsset = "";
+  commencementDate ="2019-03-11";
+  leaseNameIndividual = "";
+  lessorNameIndividual = "";
+  leasseeName = "";
+  locationIndividual = "hh";
+  otherCondition = "";
+  classOfAssetIndividual = "";
+
+
+
+
+  //2nd tab question/answer
+
+  answer1 = "";
+  answer2 = "";
+  answer3 = "";
+  answer4 = "";
+  answer5 = "";
+  answer6 = "";
+  answer6new = "";
+  answer7 = "";
+
+
+  //3rd tab
+  paymentsAt = "Beginning";
+  annualDiscountRate = "3";
+  leaseTerm = "10";
+  expectedPeriod = "10";
+  leasePayment = "2670000";
+  paymentIntervals = "Yearly";
+  initialDirectCost = "0";
+  guaranteedResidualValue = "1000000";
+  usefulLifeOfTheAsset = "10";
+  escalation = "30";
+  escalationAfterEvery = "10";
+  contractCurrency = "";
+  //mapIndividualUserDetails: Map<String, String>;
+  mapIndividualUserDetails:  Map<string, string>;
+  map: Map<string, Map<string, string>>;
+  map1: Map<String, String>;
   presentValue: number;
 
-  private setGlobals() {
-
-  }
+  constructor(public paymentService: paymentService, public globals: Globals, public rightService: rightService, public journalService: JournalService,  private spinner: NgxSpinnerService) { }
+  mapUserData: Map<string, Map<string, string>>;
+  mapUserFilter: Map<string, Map<string, string>>;
+ 
 
   public getFilterUserData() {
+    this.spinner.show();
 
 
     this.userId = localStorage.getItem('userId');
@@ -46,10 +89,9 @@ export class PaymentReportComponent implements OnInit {
     if(this.userId === "undefined"){
       this.userId = 0;
     }
-    alert($('#dateSelector').val());
     var ret = ($('#dateSelector').val().split("-"));
    
-    var day = 10
+    var day = 10;
     var year = ret[0];
     var month = ret[1];
 
@@ -61,14 +103,16 @@ export class PaymentReportComponent implements OnInit {
       year: year,
       month: month
     };
-    alert(JSON.stringify(data))
     this.journalService.calculate(data).then(response => {
-      console.log(response.data)
-      $.each(response.data, function (index) {
-        this.map = new Map(Object.entries(response.data[index]));
-        console.log(this.map)
+      this.spinner.hide();
+      this.mapUserData = new Map(Object.entries(response.data))
 
-      });
+       console.log(this.mapUserData)
+      // $.each(response.data, function (index) {
+      //   this.map = new Map(Object.entries(response.data[index]));
+      //   console.log(this.map)
+
+      // });
 
     });
 
@@ -91,12 +135,74 @@ export class PaymentReportComponent implements OnInit {
 
   }
   ngOnInit() {
-    var data = {};
-    this.rightService.getUsersData(data).then(response => {
-      this.mapUserData = new Map(Object.entries(response.data));
-      this.mapUserFilter = new Map(Object.entries(response.data));
+    var me =this
+    var globalLInk = this.globals.APP_URL
+    $("#dataListUl").on("click", ".dataListLi", function(event){
+      me.spinner.show();
+      var dataId =$(this).attr('id');
+        var data = {
+          dataId: dataId
+        };
+        me.populateUserData(me, data);
+        me.populateDataTables(me, data);
+        me.spinner.hide();
 
+  });
+    // me.spinner.show();
+    // var data = {};
+    // this.rightService.getUsersData(data).then(response => {
+    //   me.spinner.hide();
+    //   this.mapUserFilter = new Map(Object.entries(response.data));
+    //   this.mapUserData = new Map(Object.entries(response.data));
+    //   console.log(response.data)
+    // });
+  }
+
+  private populateUserData(me: this, data: { dataId: any; }) {
+    me.rightService.getIndividualReportDataByDataId(data).then(response => {
+      var userDetails = response.data;
+      me.leaseNameIndividual = userDetails.leaseName;
+      me.lessorNameIndividual = userDetails.lessorName;
+      me.classOfAssetIndividual = userDetails.classOfAsset;
+      me.leaseContractNo = userDetails.leaseContractNo;
+      me.commencementDate = userDetails.commencementDate;
+      me.locationIndividual = userDetails.location;
+
+      //for second tab
+      me.paymentsAt = userDetails.paymentsAt;
+      me.annualDiscountRate = userDetails.annualDiscountRate;
+      me.leaseTerm = userDetails.leaseTerm;
+      me.expectedPeriod = userDetails.expectedPeriod;
+      me.leasePayment = userDetails.leasePayment;
+      me.paymentIntervals = userDetails.paymentIntervals;
+      me.initialDirectCost = userDetails.initialDirectCost;
+      me.guaranteedResidualValue = userDetails.guaranteedResidualValue;
+      me.usefulLifeOfTheAsset = userDetails.usefulLifeOfTheAsset;
+      me.escalation = userDetails.escalation;
+      me.escalationAfterEvery = userDetails.escalationAfterEvery;
+      //   this.mapUserData = new Map(response.data);
+
+      //for question tab
+      me.answer1 = userDetails.answer1
+      me.answer2 = userDetails.answer2
+      me.answer3 = userDetails.answer3
+      me.answer4 = userDetails.answer4
+      me.answer5 = userDetails.answer5
+      me.answer6 = userDetails.answer6
+      me.answer7 = userDetails.answer7
+      console.log(response.data);
+    });
+  }
+
+  private populateDataTables(me: this, data: { dataId: any; }) {
+    me.rightService.calculateDataTables(data).then(response => {
+      var userDetails = response.data;
+      me.map = new Map(Object.entries(response.data));
       console.log(response.data)
+      me.map1 = this.map.get("17");
+      me.presentValue = this.map1[9];
+     
+      console.log(response.data);
     });
   }
 
