3be8d92ac8811258e656ae763a6013bb6624e082
hamza2055 <hamzariaz1994@gmail.com>
2020-12-21 19:22:03 +0500

    basic code to get param from url written

--- src/app/requeestApproved/requeestApproved.component.ts
+++ src/app/requeestApproved/requeestApproved.component.ts
@@ -1,18 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from "@angular/router";
+
 
 @Component({
   selector: 'app-requeestApproved',
   templateUrl: './requeestApproved.component.html',
   styleUrls: ['./requeestApproved.component.css']
 })
-export class RequeestApprovedComponent implements OnInit {
 
-  constructor() { }
+export class RequeestApprovedComponent implements OnInit {
+  userId = ""
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
+    console.log(this.route.snapshot.paramMap)
+    this.userId = this.route.snapshot.paramMap.get("id")
+    alert(this.userId)
     $( document ).ready(function() {
+     
       $('#mainNavBar').hide();
     });
   }
+  
 
 }
