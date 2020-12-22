6fa23f045ba6dafdd44d5a763c8f0f80be66e583
Adnan Khokhar <45161899+Adnankhokhar747@users.noreply.github.com>
2020-12-22 10:26:32 +0500

    new lease journal entry

--- src/app/newLeasejournalentry/newLeasejournalentry.component.ts
+++ src/app/newLeasejournalentry/newLeasejournalentry.component.ts
@@ -79,17 +79,6 @@ export class NewLeasejournalentryComponent implements OnInit {
       $("#journal_entry1").toggle("show");
 
     });
-    var modal = document.getElementById("myModall");
-    var btn = document.getElementById("myBtnn");
-    var span = document.getElementsByClassName("close")[0];
-    btn.onclick = function() {
-      modal.style.display = "block";
-    }
-    window.onclick = function(event) {
-      if (event.target == modal) {
-        modal.style.display = "none";
-      }
-    }
     var data = {
 
       leaseContractNo: this.globals.leaseContractNo,
