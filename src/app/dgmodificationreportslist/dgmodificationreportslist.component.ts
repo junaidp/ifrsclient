980abce5a6798a76f6d40fa4c2d1e10defbced56
hamza2055 <hamzariaz1994@gmail.com>
2021-05-05 14:09:18 +0500

    done initial front end changes for modification

--- src/app/dgmodificationreportslist/dgmodificationreportslist.component.ts
+++ src/app/dgmodificationreportslist/dgmodificationreportslist.component.ts
@@ -3,8 +3,10 @@ import { Globals } from "../globals";
 import { LeaseService } from "../new-lease/leaseService";
 import { rightService } from "./rightService";
 import { ThrowStmt } from '@angular/compiler';
-import { JsonPipe } from '@angular/common';
+import { JsonPipe, KeyValue } from '@angular/common';
 import { NgxSpinnerService } from "ngx-spinner";
+import { DisplayError } from "../displayError";
+
 declare var $: any;
 
 @Component({
@@ -14,160 +16,34 @@ declare var $: any;
 })
 export class DgmodificationreportslistComponent implements OnInit {
 
+  //mapIndividualUserDetails: Map<String, String>;
+  constructor(public rightService: rightService, public leaseService: LeaseService, public globals: Globals, private spinner: NgxSpinnerService, public displayError: DisplayError) { }
+ 
   leaseName = "All";
   lessorName = "All";
   classOfAsset = "All";
-  location = "";
-  date = "";
-  vendorName = "";
-  startingDate = "";
-  endingDate = "";
-  ///////////////////////////////// for individual report data ////////////////////
-  dataId = ""
-  leaseContractNo = "";
-  classAsset = "";
-  commencementDate = "2019-03-11";
-  leaseNameIndividual = "";
-  lessorNameIndividual = "";
-  leasseeName = "";
-  locationIndividual = "hh";
-  classOfAssetIndividual = "";
-  assetDescriptionIndividual = ""
+  location = "All";
+  assetCode="All";
+  leaseContractNo = "All";
 
-
-
-
-  //2nd tab question/answer
-
-  answer1 = "";
-  answer2 = "";
-  answer3 = "";
-  answer4 = "";
-  answer5 = "";
-  answer6 = "";
-  answer6new = "";
-  answer7 = "";
-  conclusion = "";
-
-
-  //3rd tab
-  paymentsAt = "";
-  annualDiscountRate = "";
-  leaseTerm = "";
-  expectedPeriod = "";
-  leasePayment = "";
-  paymentIntervals = "";
-  initialDirectCost = "";
-  guaranteedResidualValue = "";
-  usefulLifeOfTheAsset = "";
-  escalation = "";
-  escalationAfterEvery = "";
-  contractCurrency = "";
-  savedFileDb = ""
-  //mapIndividualUserDetails: Map<String, String>;
-  mapIndividualUserDetails: Map<string, string>;
-  map: Map<string, Map<string, string>>;
-  map1: Map<String, String>;
-  presentValue: number;
-
-  //for quetsions hide 
-  answer1Bool = true;
-  answer2Bool = true;
-  answer3Bool = true;
-  answer4Bool = true;
-  answer5Bool = true;
-  answer6Bool = true;
-  answer7Bool = true;
-
-  constructor(public rightService: rightService, public leaseService: LeaseService, public globals: Globals, private spinner: NgxSpinnerService) { }
   mapUserData: Map<string, Map<string, string>>;
-  mapIndividualUserData: Map<string, Map<string, string>>;
   mapUserFilter: Map<string, Map<string, string>>;
-  mapClassOfAsset: Map<string, Map<string, string>>;
   mapClassOfAssetFilter: Map<string, string>
-  mapLeaseNameFilter: Map<string, string>
   mapLessorNameFilter: Map<string, string>
+  mapContractReferenceFilter: Map<String, String>
+
+ 
 
 
-
-  public getLeaseFilterValues() {
-    var data = {
-      filterName: "leaseName"
-    };
-    this.rightService.getFiltersData(data).then(response => {
-      this.mapLeaseNameFilter = new Map(Object.entries(response.data));
-      console.log(this.mapLeaseNameFilter)
-    });
+  ngOnInit() {
+    this.getFilterUserData();
+    this.getClassOfAssetFilterData();
+    this.getLessorFilterData();
+    this.getReferenceNoFilterData();
   }
 
 
-  public getFile() {
-    var data = {
-      dataId: this.dataId
-    };
-    let thefile = {};
-    this.rightService.getIndividualReportFileByDataId(data).then(response => {
 
-      thefile = new Blob([response.config.url], { type: "application/vnd.openxmlformats-officedocument.wordprocessingml.document" })
-      console.log(thefile)
-    let url = (response.config.url);
-    console.log(url)
-    window.open(url);
-     // window.location.href = response.data;
-   //   this.mapLeaseNameFilter = new Map(Object.entries(response.data));
-
-
-    // var file = new Blob([response.headers], {type:  'application/docx' });
-    // var fileURL = URL.createObjectURL(file);
-    // window.open(fileURL);
-      
-    // var binaryData = [];
-    // binaryData.push(response);
-    // var url = window.URL.createObjectURL(new Blob(binaryData, {type: "application/docx"}));
-    // var a = document.createElement('a');
-    // document.body.appendChild(a);
-    // a.setAttribute('style', 'display: none');
-    // a.setAttribute('target', 'blank');
-    // a.href = url;
-    // a.download = response.data.filename;
-    // a.click();
-    // window.URL.revokeObjectURL(url);
-    // a.remove();
-
-
-    });
-  }
-
-  public getLessorFilterValues() {
-    var data = {
-      filterName: "lessorName"
-    };
-    this.rightService.getFiltersData(data).then(response => {
-      this.mapLessorNameFilter = new Map(Object.entries(response.data));
-      console.log(this.mapLessorNameFilter)
-    });
-  }
-
-  public getClassOfAssetFilterValues() {
-    var data = {
-      filterName: "classOfAsset"
-    };
-    this.rightService.getFiltersData(data).then(response => {
-      this.mapClassOfAssetFilter = new Map(Object.entries(response.data));
-      console.log(this.mapClassOfAssetFilter)
-    });
-  }
-
-  public getClassOfAsset() {
-    //  this.spinner.show();
-    var data = {};
-
-    this.leaseService.getClassOfAsset(data).then(response => {
-      //   this.spinner.hide();
-      this.mapClassOfAsset = new Map(Object.entries(response.data));
-      console.log(JSON.stringify(response.data));
-    });
-  }
 
   public getFilterUserData() {
     this.spinner.show();
@@ -175,6 +51,8 @@ export class DgmodificationreportslistComponent implements OnInit {
       leaseName: this.leaseName,
       lessorName: this.lessorName,
       classOfAsset: this.classOfAsset,
+      location: this.location,
+      assetCode: this.assetCode,
       userId: localStorage.getItem('userId'),
       companyId: localStorage.getItem('companyId')
     };
@@ -183,32 +61,66 @@ export class DgmodificationreportslistComponent implements OnInit {
       this.spinner.hide();
       this.mapUserData = new Map(Object.entries(response.data));
       console.log(response.data)
-    });
+    },
+      (error): void => {
+        //Error callback
+        this.spinner.hide();
+        this.displayError.displayErrorMessage(error);
+
+      }
+    );
   }
 
 
-  ngOnInit() {
-    this.savedFileDb ="sadasdasd"
-    // me will be used instead of this here
-    var me = this
-    //for click on delete icon for deleting userdara
-    this.deleteUsersData(me);
 
+  public getClassOfAssetFilterData() {
+    var data = {
+      filterName: "classOfAsset"
+    };
+    this.rightService.getFiltersData(data).then(response => {
+      this.mapClassOfAssetFilter = new Map(Object.entries(response.data));
+      console.log(this.mapClassOfAssetFilter)
+    },
+      (error): void => {
+        //Error callback
+        this.spinner.hide();
+        this.displayError.displayErrorMessage(error);
 
-    this.getClassOfAssetFilterValues();
-    this.getLeaseFilterValues();
-    this.getLessorFilterValues();
+      }
+    );
+  }
+  public getLessorFilterData() {
+    var data = {
+      filterName: "lessorName"
+    };
+    this.rightService.getFiltersData(data).then(response => {
+      this.mapLessorNameFilter = new Map(Object.entries(response.data));
+      console.log(this.mapLessorNameFilter)
+    },
+    (error): void => {
+      //Error callback
+      this.spinner.hide();
+      this.displayError.displayErrorMessage(error);
 
-    // for opening of userDetails modal wile clicking on details
-    this.openUserDetailModal(me);
+    }
+    );
+  }
 
-    var globalLInk = this.globals.APP_URL
+  public getReferenceNoFilterData() {
+    var data = {
+      filterName: "leaseContractNo"
+    };
+    this.rightService.getFiltersData(data).then(response => {
+      this.mapContractReferenceFilter = new Map(Object.entries(response.data));
+      console.log(this.mapLessorNameFilter)
+    },
+    (error): void => {
+      //Error callback
+      this.spinner.hide();
+      this.displayError.displayErrorMessage(error);
 
-    $('#confirm').on('click', function () {
-      confirm("Press a button!");
-    });
-    me.spinner.show();
-    this.populateInitialUsersData(me);
+    }
+    );
   }
 
 
@@ -219,160 +131,16 @@ export class DgmodificationreportslistComponent implements OnInit {
       var data = {
         dataId: dataId
       };
-      me.populateUserData(me, data);
-      me.populateDataTables(me, data);
+      
       me.spinner.hide();
 
     });
   }
 
-  private deleteUsersData(me: this) {
-    $("#dataListUl").on("click", ".dataListLi .row .deleteLease .deleteLeaseIcon", function (event) {
-      if (confirm("Press okay if you want to delete this!")) {
-        var leaseId = $(this).attr('id');
 
-        var data = {
-          leaseId: leaseId
-        };
-        //alert(data)
-        me.rightService.deleteSelectedLease(data).then(response => {
-          // this.mapLessorNameFilter = new Map(Object.entries(response.data));
-          console.log(response.data);
-
-          var msg;
-          if (response.data.includes("Fail")) {
-            msg = '<div class="alert alert-danger"  role="alert" >' + response.data + '</div>';
-            $('#rightOfUseResponsePanel').html(msg);
-            setTimeout(function () {
-              $('#rightOfUseResponsePanel .alert').slideToggle();
-            }, 6000);
-
-          }
-
-          else {
-            msg = '<div class="alert alert-info"  role="alert" >' + response.data + '</div>';
-            $('#rightOfUseResponsePanel').html(msg);
-            setTimeout(function () {
-              $('#rightOfUseResponsePanel .alert').slideToggle();
-            }, 6000);
-          }
-
-
-          me.populateInitialUsersData(me);
-          me.getLeaseFilterValues();
-          me.getLessorFilterValues();
-          me.getClassOfAssetFilterValues();
-        });
-      }
-
-    });
+  originalOrder = (a: KeyValue<string, Map<string, string>>, b: KeyValue<string, Map<string, string>>): number => {
+    return 0;
   }
 
-  private populateInitialUsersData(me: this) {
-    var data = {};
-    this.rightService.getUsersData(data).then(response => {
-      me.spinner.hide();
-      this.mapUserFilter = new Map(Object.entries(response.data));
-      this.mapUserData = new Map(Object.entries(response.data));
-      console.log(response.data);
-    });
-  }
-
-  private populateUserData(me: this, data: { dataId: any; }) {
-    me.rightService.getIndividualReportDataByDataId(data).then(response => {
-      var userDetails = response.data;
-      me.dataId = userDetails.id
-      me.leaseNameIndividual = userDetails.leaseName;
-      me.lessorNameIndividual = userDetails.lessorName;
-      me.classOfAssetIndividual = userDetails.classOfAsset;
-      me.leaseContractNo = userDetails.leaseContractNo;
-      me.commencementDate = userDetails.commencementDate;
-      me.locationIndividual = userDetails.location;
-      me.assetDescriptionIndividual = userDetails.assetDescription;
-
-      //for second tab
-      me.paymentsAt = userDetails.paymentsAt;
-      me.annualDiscountRate = userDetails.annualDiscountRate;
-      me.leaseTerm = userDetails.leaseTerm;
-      me.expectedPeriod = userDetails.expectedPeriod;
-      me.leasePayment = userDetails.leasePayment;
-      me.paymentIntervals = userDetails.paymentIntervals;
-      me.initialDirectCost = userDetails.initialDirectCost;
-      me.guaranteedResidualValue = userDetails.guaranteedResidualValue;
-      me.usefulLifeOfTheAsset = userDetails.usefulLifeOfTheAsset;
-      me.escalation = userDetails.escalation;
-      me.escalationAfterEvery = userDetails.escalationAfterEvery;
-      //   this.mapUserData = new Map(response.data);
-
-      //for question tab
-      me.answer1 = userDetails.answer1
-      me.answer2 = userDetails.answer2
-      me.answer3 = userDetails.answer3
-      me.answer4 = userDetails.answer4
-      me.answer5 = userDetails.answer5
-      me.answer6 = userDetails.answer6
-      me.answer7 = userDetails.answer7
-      me.conclusion = userDetails.conclusion,
-        me.contractCurrency = userDetails.contractCurrency
-        me.savedFileDb = userDetails.fileName
-      console.log(response.data);
-
-      if (me.answer1 == "") {
-        me.answer1Bool = false;
-      }
-      else {
-        me.answer1Bool = true;
-      }
-      if (me.answer2 == "") {
-        me.answer2Bool = false;
-      }
-      else {
-        me.answer2Bool = true;
-      }
-      if (me.answer3 == "") {
-        me.answer3Bool = false;
-      }
-      else {
-        me.answer3Bool = true;
-      }
-      if (me.answer4 == "") {
-        me.answer4Bool = false;
-      }
-      else {
-        me.answer4Bool = true;
-      }
-      if (me.answer5 == "") {
-        me.answer5Bool = false;
-      }
-      else {
-        me.answer5Bool = true;
-      }
-      if (me.answer6 == "") {
-        me.answer6Bool = false;
-      }
-      else {
-        me.answer6Bool = true;
-      }
-      if (me.answer7 == "") {
-        me.answer7Bool = false;
-      }
-      else {
-        me.answer7Bool = true;
-      }
-
-    });
-  }
-
-  private populateDataTables(me: this, data: { dataId: any; }) {
-    me.rightService.calculateDataTables(data).then(response => {
-      var userDetails = response.data;
-      me.map = new Map(Object.entries(response.data));
-      console.log(response.data)
-      me.map1 = this.map.get("17");
-      me.presentValue = this.map1[9];
-
-      console.log(response.data);
-    });
-  }
 }
 
