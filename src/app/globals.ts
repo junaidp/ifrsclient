0ee8084115c39c4a9af63880b844ad32ceaf1148
hamza2055 <hamzariaz1994@gmail.com>
2021-10-06 13:49:57 +0500

    global link changed

--- src/app/globals.ts
+++ src/app/globals.ts
@@ -5,8 +5,9 @@ import { Injectable } from '@angular/core';
 //3a03924fb95b
 //ea902d90105c
 export class Globals {
+
   
-  APP_URL  = "http://6cd4-206-84-133-75.ngrok.io/";
+  APP_URL  = "http://c28b-101-50-88-15.ngrok.io/";
   //APP_URL  = "//7d83bbbe.ngrok.io";
   APP_URL_HEROKU = "//compliancetool.herokuapp.com";
   reportRighOfUseRoute = '/reports/rightofuse'
