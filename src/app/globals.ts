43b65aa46adae81650e487f877b94beb8eeaeb98
junaid <junaidp@gmail.com>
2022-05-24 08:10:26 +0200

    Update globals.ts

--- src/app/globals.ts
+++ src/app/globals.ts
@@ -7,7 +7,7 @@ import { Injectable } from '@angular/core';
 export class Globals {
 
 
-  APP_URL  = "https://a00a-101-50-88-15.ngrok.io/";
+  APP_URL  = "https://0b52-203-175-71-2.ngrok.io/";
   //APP_URL  = "//7d83bbbe.ngrok.io";
   APP_URL_HEROKU = "//compliancetool.herokuapp.com";
   reportRighOfUseRoute = '/reports/rightofuse'
